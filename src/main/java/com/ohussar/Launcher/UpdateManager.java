1c603a28eb71ab08bedd3885600b0d2dfb2e8705
ohussar <pedrogabrielhussar133@gmail.com>
2025-12-12 23:07:24 -0300

    fix

--- src/main/java/com/ohussar/Launcher/UpdateManager.java
+++ src/main/java/com/ohussar/Launcher/UpdateManager.java
@@ -10,6 +10,7 @@ import java.net.URISyntaxException;
 public class UpdateManager {
 
     public static final String checkUrl = "https://api.github.com/repos/ohussar/moddedlauncher2025-2";
+    public static final String downloadUrl = "https://raw.githubusercontent.com/ohussar/moddedlauncher2025-2/master/release/launcher.jar";
 
     public static void checkForUpdates(){
         JsonElement element;
@@ -25,7 +26,11 @@ public class UpdateManager {
                 String d = desc.getAsString();
 
                 if(!Main.version.equals(d)){
-
+                    try {
+                        HttpRequester.download(downloadUrl, "launcherD.jar", ".", (b) -> {});
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
                 }
             }
         }
--- src/main/java/com/ohussar/Main.java
+++ src/main/java/com/ohussar/Main.java
@@ -24,7 +24,7 @@ public class Main {
 
     private static final boolean isDevEnviroment = true;
 
-    public static final String version = "1.0";
+    public static final String version = "1.1";
 
     public static String rootPath = ".";
     public static String minecraftPath = PathMaker.buildPath(rootPath, "Minecraft", ".minecraft");
